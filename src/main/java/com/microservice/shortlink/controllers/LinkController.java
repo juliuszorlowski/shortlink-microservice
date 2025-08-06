847ca41f27096c0f98d3856787394a1fb6e751a9
Juliusz Orłowski <orlowski.juliusz@gmail.com>
2025-08-07 00:28:28 +0200

    Added redirectToLink endpoint

--- src/main/java/com/microservice/shortlink/controllers/LinkController.java
+++ src/main/java/com/microservice/shortlink/controllers/LinkController.java
@@ -3,27 +3,33 @@ package com.microservice.shortlink.controllers;
 import com.microservice.shortlink.dtos.CodeDto;
 import com.microservice.shortlink.dtos.ShortenLinkRequest;
 import com.microservice.shortlink.entities.Link;
-import com.microservice.shortlink.mappers.LinkMapper;
 import com.microservice.shortlink.repositories.LinkRepository;
 import jakarta.validation.Valid;
-import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.net.URI;
 import java.time.LocalDateTime;
 import java.util.Random;
 
 @RestController
-@AllArgsConstructor
 @RequestMapping()
 public class LinkController {
+    @Value("${app-url}")
+    private String appUrl;
+
     private final LinkRepository linkRepository;
+
+    public LinkController(LinkRepository linkRepository) {
+        this.linkRepository = linkRepository;
+    }
 
     @PostMapping("/shorten")
     public ResponseEntity<?> submitLink(@Valid @RequestBody ShortenLinkRequest request) {
         var code = generateRandomString();
         var date = LocalDateTime.now();
-        System.out.println(date);
         var link = Link.builder()
                 .url(request.getUrl())
                 .code(code)
@@ -33,10 +39,25 @@ public class LinkController {
 
         linkRepository.save(link);
         CodeDto codeDto = new CodeDto();
-        codeDto.setShortened_url(code);
-        System.out.println(link.getCreatedAt());
+        codeDto.setShortened_url(appUrl + code);
 
         return ResponseEntity.ok(codeDto);
+    }
+
+    @GetMapping("/{code}")
+    public ResponseEntity<Void> redirectToLink(@PathVariable String code) {
+        var link = linkRepository.findByCode(code).orElse(null);
+        if (link == null) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+        link.setClickCount(link.getClickCount() + 1);
+        linkRepository.save(link);
+        var url = link.getUrl();
+
+        return ResponseEntity
+                .status(HttpStatus.FOUND)
+                .location(URI.create(url))
+                .build();
     }
 
     private String generateRandomString() {
--- src/main/java/com/microservice/shortlink/repositories/LinkRepository.java
+++ src/main/java/com/microservice/shortlink/repositories/LinkRepository.java
@@ -3,5 +3,8 @@ package com.microservice.shortlink.repositories;
 import com.microservice.shortlink.entities.Link;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface LinkRepository extends JpaRepository<Link, Long> {
+    Optional<Link> findByCode(String code);
 }
